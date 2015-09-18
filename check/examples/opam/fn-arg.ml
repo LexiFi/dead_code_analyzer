ee25bd5f96aa38d4ecb6feaf6a4681fbca76d007
cdesouza <corentin.de.souza@lexifi.com>
2015-09-18 12:55:19 +0200

    test-suite position detecting more accurate

--- check/examples/opam/fn-arg.ml
+++ check/examples/opam/fn-arg.ml
@@ -4,7 +4,7 @@
 ./examples/opam/fn-arg.ml:3: b ALWAYS
 ./examples/opam/fn-arg.ml:7: a ALWAYS
 ./examples/opam/fn-arg.ml:7: b NEVER
-./examples/opam/fn-arg.ml:9: b ALWAYS
 ./examples/opam/fn-arg.ml:9: c ALWAYS
+./examples/opam/fn-arg.ml:9: b ALWAYS
 ./examples/opam/fn-arg.ml:11: b ALWAYS
 ./examples/opam/fn-arg.ml:11: b ALWAYS
