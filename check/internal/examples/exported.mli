6f842dcd1be1836626c02227a6057cf9ce5468f6
cdesouza <corentin.de.souza@lexifi.com>
2016-01-04 14:27:53 +0100

    new example: some not exactly false things to fix

---
+++ check/internal/examples/exported.mli
@@ -0,0 +1,3 @@
+./examples/exported.ml:13: F.times_five
+./examples/exported.ml:18: M2.five
+./examples/exported.ml:20: M2.ten
---
+++ examples/exported.ml
@@ -0,0 +1,30 @@
+module type T = sig
+  val five : int
+end
+
+
+module M1 : T = struct
+  let five = 5
+end
+
+
+module F (M : T) = struct
+  let plus_five n = n + M.five
+  let times_five n = n * M.five
+end
+
+
+module M2 : sig
+  val five : int
+  val plus_five : int -> int
+  val ten : int
+end = struct
+  include M1
+  include F(M1)
+  let ten = plus_five 5
+end
+
+
+let () =
+  let twenty = M2.plus_five 5 + M2.plus_five 5 in
+  print_int twenty
