a9e0a38149f60aceec82e4581fcd15a5615ab554
cdesouza <corentin.de.souza@lexifi.com>
2016-01-12 13:38:39 +0100

    2015 -> 2016

--- src/deadArg.mli
+++ src/deadArg.mli
@@ -1,6 +1,6 @@
 (***************************************************************************)
 (*                                                                         *)
-(**  Copyright (c) 2014-2015 LexiFi SAS. All rights reserved.              *)
+(**  Copyright (c) 2014-2016 LexiFi SAS. All rights reserved.              *)
 (*                                                                         *)
 (*   This source code is licensed under the ISC License                    *)
 (*   found in the LICENSE file at the root of this source tree             *)
