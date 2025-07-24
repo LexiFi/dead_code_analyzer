542f2fa74607c3fa7555f8b994cb692dc3ba5623
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2025-07-24 14:04:23 +0200

    [functor] fix FP on functor application
    Any value in a functor's argument expected by the module type of
    the corresponding formal parameter is considered use because it is
    required

--- src/deadMod.ml
+++ src/deadMod.ml
@@ -17,36 +17,38 @@ open DeadCommon
 let defined : string list ref = ref []
 
 
-let rec sign = function
+let rec sign ?(select_param = false) = function
   | Mty_signature sg -> sg
-  | Mty_functor (_, t) -> sign t
+  | Mty_functor (_, t) when not select_param -> sign t
+  | Mty_functor (Named (_, t), _) -> sign t
   | _ -> []
 
 
 let item maker = function
   | Sig_value (id, {val_loc = {Location.loc_start= loc; _}; _}, _) ->
-      (Ident.name id, loc)::[]
+    (Ident.name id, loc)::[]
   | Sig_type (id, {type_kind; _}, _, _) ->
     let t = Ident.name id in
     begin match type_kind with
     | Type_record (l, _) ->
-        List.map
-          (fun {Types.ld_id; ld_loc = {Location.loc_start = loc; _}; _} ->
-            (t ^ "." ^ (Ident.name ld_id), loc)
-          )
-          l
+      List.map
+        (fun {Types.ld_id; ld_loc = {Location.loc_start = loc; _}; _} ->
+          (t ^ "." ^ (Ident.name ld_id), loc)
+        )
+        l
     | Type_variant (l, _) ->
-        List.map
-          (fun {Types.cd_id; cd_loc = {Location.loc_start = loc; _}; _} ->
-            (t ^ "." ^ Ident.name cd_id, loc)
-          )
-          l
-    | _ -> [] end
+      List.map
+        (fun {Types.cd_id; cd_loc = {Location.loc_start = loc; _}; _} ->
+          (t ^ "." ^ Ident.name cd_id, loc)
+        )
+        l
+    | _ -> []
+    end
   | Sig_module (id, _, {md_type; _}, _, _)
   | Sig_modtype (id, {mtd_type = Some md_type; _}, _) ->
-      List.map (fun (n, l) -> (Ident.name id ^ "." ^ n, l)) (maker md_type)
+    List.map (fun (n, l) -> (Ident.name id ^ "." ^ n, l)) (maker md_type)
   | Sig_class (id, {cty_loc = {Location.loc_start = loc; _}; _}, _, _) ->
-      (Ident.name id ^ "#", loc) :: []
+    (Ident.name id ^ "#", loc) :: []
   | _ -> []
 
 let rec make_content typ =
@@ -55,25 +57,28 @@ let rec make_content typ =
 
 
 let rec make_arg typ =
-  List.map (item make_arg) (sign typ)
+  List.map (item make_arg) (sign ~select_param:true typ)
   |> List.flatten
 
 
 let expr m = match m.mod_desc with
   | Tmod_apply (m1, m2, _) ->
-      let l1 = make_arg m1.mod_type |> List.map (fun (x, _) -> x) in
-      let l2 = make_content m2.mod_type in
-      List.iter
-        (fun (x, loc) ->
-          let is_obj = String.contains x '#' in
-          let is_type = not is_obj && DeadType.is_type x in
-          if (List.mem x l1 || l1 = [])
-          && (is_obj && !DeadFlag.obj.DeadFlag.print
-              || not is_obj && is_type && exported DeadFlag.typ loc
-              || not is_obj && not is_type && exported DeadFlag.exported loc) then
-            LocHash.add_set references loc m.mod_loc.Location.loc_start
-        )
-        l2
+    let l1 = make_arg m1.mod_type |> List.map fst in
+    let l2 = make_content m2.mod_type in
+    List.iter
+      (fun (x, loc) ->
+        let is_obj = String.contains x '#' in
+        let is_type = not is_obj && DeadType.is_type x in
+        let relevant_report_enabled =
+          if is_obj then !DeadFlag.obj.DeadFlag.print
+          else if is_type then exported DeadFlag.typ loc
+          else exported DeadFlag.exported loc
+        in
+        let value_is_expected_by_modtype = List.mem x l1 || l1 = [] in
+        if value_is_expected_by_modtype && relevant_report_enabled then
+          LocHash.add_set references loc m.mod_loc.Location.loc_start
+      )
+      l2
   | _ -> ()
 
 
