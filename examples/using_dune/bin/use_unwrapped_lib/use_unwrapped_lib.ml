98675e6c048188d6e5f71e05104f24fc4f7a665e
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2025-11-25 15:50:17 +0100

    [examples][dune] move all unwrapped_lib uses in a subdir
    This is in preparation of adding a `wrapped_lib` that will follow the
    same architecture and usage as `unwrapped_lib`. Having both mixed
    together in `bin/` would be too noisy. Hence, creating a
    `bin/use_unwrapped_lib` subdir will isolate their use. Later, a
    `bin/use_wrapped_lib` will do as well.

---
+++ examples/using_dune/bin/use_unwrapped_lib/use_unwrapped_lib.ml
@@ -0,0 +1,25 @@
+let () =
+  Use_values.mark_used ();
+  Use_values_no_intf.mark_used ();
+  Use_values_in_submodules.mark_used ();
+  Use_values_in_submodules_no_intf.mark_used ()
+
+let () =
+  Use_constructors.mark_used ();
+  Use_records.mark_used ()
+
+let () =
+  Use_opt_args.mark_used ();
+  Use_mixed_args.mark_used ();
+  Use_opt_args_in_higher_order_fun.mark_used ()
+
+let () =
+  Use_without_class.mark_used ();
+  Use_with_class.mark_used ()
+
+let () =
+  Use_style.mark_used ()
+
+let is_used = ref false
+let mark_used () =
+  is_used := true
