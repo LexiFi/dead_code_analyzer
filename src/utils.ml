c84073e86ea02340611d65df569846488140ece9
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2025-12-06 19:15:52 +0100

    [src][utils] move `Mod.sign` to `Utils.signature_of_modtype`
    This will be used in by the state component

--- src/utils.ml
+++ src/utils.ml
@@ -31,4 +31,12 @@ module Filepath = struct
     else Ignore
 end
 
+let rec signature_of_modtype ?(select_param = false) modtype =
+  let open Types in
+  match modtype with
+  | Mty_signature sg -> sg
+  | Mty_functor (_, t) when not select_param -> signature_of_modtype t
+  | Mty_functor (Named (_, t), _) -> signature_of_modtype t
+  | _ -> []
+
 module StringSet = Set.Make(String)
