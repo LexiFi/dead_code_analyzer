cdb07209395bd7165a57ba284cf9000554dc9e59
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2025-12-08 19:12:40 +0100

    [src][input][3/n] store uid's loc for values
    Using information available in the `.cmt`, store locations of uids
    coming from `include`s in a new state field :
    `state.signature.uid_to_loc`. Using `.cmt` and `.cmti`, store locations
    of values' (and patterns in value bindings) uids at the same location
    for easier lookup.
    This is done as soon as the files are loaded, before the analysis.
    
    The new table tries to be semantically equivalent to the usage of
    `cmt_value_dependencies` instead of `cmt_declaration_dependencies`
    before OCaml 5.3.
    
    This fixes the FP and FN introduced when moving to OCaml 5.3.

--- src/state/state.ml
+++ src/state/state.ml
@@ -3,11 +3,13 @@ module File_infos = File_infos
 type t =
   { config : Config.t
   ; file_infos : File_infos.t
+  ; signature : Signature.t
   }
 
 let init config =
   { config
   ; file_infos = File_infos.empty
+  ; signature = Signature.empty ()
   }
 
 let update_config config state =
@@ -18,21 +20,24 @@ let change_file state cm_file =
   let equal_no_ext filename1 filename2 =
     let no_ext1 = Filename.remove_extension filename1 in
     let no_ext2 = Filename.remove_extension filename2 in
-    no_ext1 = no_ext2
+    String.equal no_ext1 no_ext2
   in
-  if file_infos.cm_file = cm_file then
+  if String.equal file_infos.cm_file cm_file then
     Result.ok state
   else if equal_no_ext file_infos.cm_file cm_file then
     let file_infos = File_infos.change_file file_infos cm_file in
     Result.map (fun file_infos -> {state with file_infos}) file_infos
   else
-    let file_infos = File_infos.init cm_file in
-    Result.map (fun file_infos -> {state with file_infos}) file_infos
+    let ( let* ) x f = Result.bind x f in
+    let* file_infos = File_infos.init cm_file in
+    let* signature = Signature.init file_infos in
+    Result.ok {state with file_infos; signature}
 
 (** Analysis' state *)
 let current = ref
     { config = Config.default_config
     ; file_infos = File_infos.empty
+    ; signature = Signature.empty ()
     }
 
 let get_current () = !current
