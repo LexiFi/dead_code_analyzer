df806ba4687529d588850eab7db80a4746e211fb
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2025-12-21 16:53:58 +0100

    [tests][dune] add tests with preprocessing
    `examples/using_dune/lib/preprocessed/` contains minimal examples with
    preprocessed files : `preprocessed.ml`, `preprocessed.mli`, and
    `preprocessed_no_intf.ml`. The `.ml` files are identical. Most of the
    content of the files are not affected by preprocessing. Only 2 types
    are : `constr_with_eq` and `record_with_eq`. The former exposes a
    constructor that will be matched in the generated
    `equal_constr_with_eq`. The latter exposes a field that will be read by
    the generated `equal_record_with_eq`. Consequently, the constructor
    remains unused but the field becomes used.
    
    As shown by the updated `.ref` files, preprocessed files introduce FN
    and FP.

---
+++ examples/using_dune/preprocessed_lib/preprocessed.ml
@@ -0,0 +1,68 @@
+(* Values *)
+let unused = 42
+let used = 42
+let internally_used = 42
+let externally_used = 42
+
+let () = (* use values *)
+  ignore used;
+  ignore internally_used
+
+(* Methods *)
+let immediate = object
+  method unused = 42
+  method used = 42
+  method internally_used = 42
+  method externally_used = 42
+end
+
+let () = (* use methods *)
+  ignore immediate#used;
+  ignore immediate#internally_used
+
+(* Constructors *)
+type constructors =
+  | Unused
+  | Used
+  | Internally_used
+  | Externally_used
+
+type constr_with_eq = Unused
+[@@deriving eq]
+
+let () = (* use constructors *)
+  ignore Used;
+  ignore Internally_used
+
+(* Record fields *)
+type record = {
+  unused : int;
+  used : int;
+  internally_used : int;
+  externally_used : int;
+}
+
+type record_with_eq = {implicitly_used : int} [@@deriving eq]
+
+let () = (* use record fields *)
+  let r = {unused = 42; used = 42; internally_used = 42; externally_used = 42} in
+  ignore r.used;
+  ignore r.internally_used
+
+(* Optional arguments *)
+let f ?never ?always ?internally ?externally () =
+  ignore never;
+  ignore always;
+  ignore internally;
+  ignore externally
+
+let () = (* use optional arguments *)
+  f ~always:42 ~internally:42 ()
+
+(* Stylistic issues *)
+let _ =
+  let unit_binding = () in
+  let expect_opt_arg_in_arg (f : ?opt:'a -> unit -> unit) = f () in
+  let () (* sequence *) = ignore expect_opt_arg_in_arg in
+  let useless_binding = 42 in
+  useless_binding
