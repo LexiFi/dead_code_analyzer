71ecf02aeef8b95ee9eb6322515eda29f061ac08
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2026-01-28 14:48:06 +0100

    [examples][dune] add tests for internal module type inclusion
    Instead of including a modtype from an external module in the signature,
    the same module both defines and includes a modtype in its signature.
    
    The current report semantic for the values in module types is to not
    report them.

--- examples/using_dune/bin/use_unwrapped_lib/use_include_modtype.ml
+++ examples/using_dune/bin/use_unwrapped_lib/use_include_modtype.ml
@@ -9,6 +9,11 @@ let () =
   ignore Imt_include_use_less.used;
   ignore Imt_include_use_less.externally_used
 
+(* use Imt_internal_include *)
+let () =
+  ignore Imt_internal_include.used;
+  ignore Imt_internal_include.externally_used
+
 let is_used = ref false
 let mark_used () =
   is_used := true
