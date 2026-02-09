88d4772f793c3b2a36884de37651bb557cd7e19b
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2026-02-09 19:25:36 +0100

    [docs][exported_values][1/n] initial documentation
    Provide definitions and basic usage information about exported unused
    values.
    Provide very basic examples without any external use, with and without
    .mli. The examples are walkthrough of the usage of the analyzer on
    actual code.

---
+++ docs/exported_values/hello_world.ml
@@ -0,0 +1,9 @@
+(* hello_world.ml *)
+let hello = "Hello"
+let goodbye = "Goodbye"
+let world = "World"
+
+let () =
+  let hello_world = hello ^ world in
+  let goodbye_world = goodbye ^ world in
+  print_endline hello_world
